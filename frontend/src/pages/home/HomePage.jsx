b33313462a5a69cd42a408c438424fe7535ac128
tizbami <aurrietadah@gmail.com>
2025-01-28 18:00:17 +0100

    hero fixed

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -1,21 +1,9 @@
-// // import { useAuthStore } from "../../authUser";
-// import AuthScreen from "./AuthScreen";
-// import HomeScreen from "./HomeScreen";
-
-// const HomePage = () => {
-//   const { user } = useAuthStore();
-
-//   return <>{user ? <HomeScreen /> : <AuthScreen />}</>;
-// };
-// export default HomePage;
-
 import AuthScreen from "./AuthScreen";
 import HomeScreen from "./HomeScreen";
 const HomePage = () => {
   const user = false;
 
   return <div>{user ? <HomeScreen /> : <AuthScreen />}</div>;
-     
 };
 
 export default HomePage;
